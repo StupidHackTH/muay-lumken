6172c0b37d81ee181949b0bdd60446212af130d7
Pmc Dev <phoomparin@gmail.com>
2018-07-15 03:42:49 +0700

    :sparkles: Generate Pattern for One Constraint

--- src/generate/rule.js
+++ src/generate/rule.js
@@ -3,20 +3,45 @@ import RandExp from 'randexp'
 import parse from './parser'
 import ruleset from './ruleset'
 
+const randomize = list => list[Math.floor(Math.random() * list.length)]
+
+function calculatePadding(constraints) {
+  const total = 8
+  const data = constraints.length * 2
+  const usable = total - data
+  const start = Math.round(Math.random() * usable)
+  const end = usable - start
+
+  return [start, data, end]
+}
+
+function generatePattern(constraints) {
+  const [start, mid, end] = calculatePadding(constraints)
+
+  return '\\d{' + start + '}' + constraints.join('|') + '\\d{' + end + '}'
+}
+
 class RuleEngine {
   constructor(ruleset) {
     this.ruleset = ruleset
   }
 
-  generate(constraint) {
-    const pattern = this.buildPattern(constraint)
-    const generator = new RandExp(pattern)
+  generate(...reqs) {
+    const {summation} = this.ruleset
+
+    const rules = reqs.map(req => this.ruleset.rules[req])
+
+    const constraints = rules.map(rule => rule.includes).map(randomize)
+
+    const pattern = generatePattern(constraints)
+
+    console.log('Rules:', rules, 'Constraints:', constraints)
+    console.log('Pattern:', pattern)
+
+    const regex = new RegExp(`0[689](${pattern})`)
+    const generator = new RandExp(regex)
 
     return generator.gen()
-  }
-
-  buildPattern(constraint) {
-    return /0[689]\d{8}/
   }
 
   parse(number) {
